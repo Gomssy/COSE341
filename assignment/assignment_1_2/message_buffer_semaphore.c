7bbd60666873f140076388f8a131c1f72a701266
Gomssy <gggomksy@naver.com>
2022-05-08 16:22:28 +0000

    1-2

--- assignment/assignment_1_2/message_buffer_semaphore.c
+++ assignment/assignment_1_2/message_buffer_semaphore.c
@@ -17,7 +17,19 @@ void init_sem() {
     /*---------------------------------------*/
     /* TODO 1 : init semaphore               */
 
-    {}
+    if((semid = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | 0666)) == -1)
+    {
+	if ((semid = semget(SEM_KEY, 0, 0)) == -1)
+	{
+	     printf("semget error!");
+	     return;
+	}
+    }
+    else
+    {
+	sem_union.val = 1;
+	semctl(semid, 0, SETVAL, sem_union);
+    }
 
     /* TODO 1 : END                          */
     /*---------------------------------------*/
@@ -28,8 +40,12 @@ void destroy_sem() {
     /*---------------------------------------*/
     /* TODO 2 : destroy semaphore            */
 
-    {}
-
+    if(semctl(semid, 0, IPC_RMID) == -1)
+    {
+    	printf("semctl error!");
+	return;
+    }
+    
     /* TODO 2 : END                          */
     /*---------------------------------------*/
     printf("destroy semid : %d\n", semid);
@@ -65,7 +81,101 @@ void s_quit() {
 /* (2) use semaphore                           */
 /* (3) report explanation (ex. 2020020000.pdf) */
 
-    {}
+
+int init_buffer(MessageBuffer **buffer) {
+
+    shmid = shmget(KEY, sizeof(MessageBuffer), IPC_CREAT|0666);
+    if(shmid == -1)
+	    return -1;
+    memory_segment = shmat(shmid, NULL, 0);
+    if(memory_segment == (void*)-1)
+	    return -1;
+    *buffer = (MessageBuffer *)memory_segment;
+
+    (*buffer)->is_empty = 1;    
+    (*buffer)->account_id = -1;
+
+    printf("init buffer\n");
+    return 0;
+}
+
+int attach_buffer(MessageBuffer **buffer) {
+   
+    if((shmid = shmget(KEY, sizeof(int), IPC_CREAT|0666)) == -1)
+	    return -1;
+		    
+    if((memory_segment = shmat(shmid, NULL, 0)) == (void *)-1)
+	    return -1;
+    *buffer = (MessageBuffer *)memory_segment;
+
+    printf("attach buffer\n");
+    printf("\n");
+    return 0;
+}
+
+int detach_buffer() {
+    if (shmdt(memory_segment) == -1) {
+        printf("shmdt error!\n\n");
+        return -1;
+    }
+
+    printf("detach buffer\n\n");
+    return 0;
+}
+
+int destroy_buffer() {
+    if(shmctl(shmid, IPC_RMID, NULL) == -1) {
+        printf("shmctl error!\n\n");
+        return -1;
+    }
+
+    printf("destroy shared_memory\n\n");
+    return 0;
+}
+
+int produce(MessageBuffer **buffer, int sender_id, int data, int account_id) {
+
+    if((account_id < 0) || (account_id > BUFFER_SIZE))
+    {
+	    return -1;
+    }
+    else if((*buffer)->messages[account_id].data + data < 0)
+    {
+	    return -1;
+    }
+    else
+    {
+	s_wait();
+
+    	(*buffer)->account_id = account_id; 
+   	(*buffer)->messages[account_id].data = data;
+    	(*buffer)->messages[account_id].sender_id = sender_id;
+
+    	(*buffer)->is_empty--;
+
+	s_quit();
+    }
+
+    printf("produce message\n");
+
+    return 0;
+}
+
+
+int consume(MessageBuffer **buffer, Message **message) {
+    if((*buffer)->is_empty)
+	return -1;
+
+    s_wait();
+
+    (*message)->data = (*buffer)->messages[(*buffer)->account_id].data;
+    (*message)->sender_id = (*buffer)->messages[(*buffer)->account_id].sender_id;
+    (*buffer)->is_empty++;
+
+    s_quit();
+    return 0;
+}
 
 /* TODO 3 : END                                */
 /*---------------------------------------------*/
+
